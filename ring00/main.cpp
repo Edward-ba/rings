212d0b13038e20fc33b6095ea920748ff62f2821
Edward-Ba <62915397+Edward-ba@users.noreply.github.com>
2024-04-27 21:53:27 -0700

    [no locks] lockless ring buffer

--- ring00/main.cpp
+++ ring00/main.cpp
@@ -2,6 +2,9 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <chrono>
+#include <vector>
+#include <stdlib.h>
 
 const int BUFFER_SIZE = 10;
 
@@ -9,61 +12,103 @@ template<typename T>
 class RingBuffer {
 private:
     T buffer[BUFFER_SIZE];
-    int head;
-    int tail;
-    std::mutex mtx;
-    std::condition_variable not_full;
-    std::condition_variable not_empty;
+    int head_;
+    int tail_;
 
 public:
-    RingBuffer() : head(0), tail(0) {}
+    RingBuffer() : head_(0), tail_(0) {}
 
     void put(const T& item) {
-        std::unique_lock<std::mutex> lock(mtx);
+        while ((head_ + 1) % BUFFER_SIZE == tail_) {}
         
-        // lamda
-        not_full.wait(lock, [this] () 
-            { return (head + 1) % BUFFER_SIZE != tail; }
-        );
-        
-        buffer[head] = item;
-        head = (head + 1) % BUFFER_SIZE;
-        not_empty.notify_one();
+        buffer[head_] = item;
+        head_ = (head_ + 1) % BUFFER_SIZE;
+        // std::cout << "head_ " << head << std::endl;
     }
 
-    T get() {
-        std::unique_lock<std::mutex> lock(mtx);
-        not_empty.wait(lock, [this]() { return head != tail; });
-        T item = buffer[tail];
-        tail = (tail + 1) % BUFFER_SIZE;
-        not_full.notify_one();
-        return item;
+    void get(std::vector<T>& items) {
+        while (head_ == tail_) {}
+        int size = (head_ - tail_ + BUFFER_SIZE) % BUFFER_SIZE;
+        // std::cout << "size " << size << std::endl;
+        items.resize(size);
+        int i = 0;
+        while (i < size) {
+            items[i] = buffer[tail_];
+            i += 1;
+            tail_ = (tail_ + 1) % BUFFER_SIZE;
+        }
+
+        // std::cout << "tail " << tail_ << std::endl;
+        // return items;
     }
 };
 
-void producer(RingBuffer<int>& buffer) {
-    for (int i = 0; i < 20; ++i) {
-        buffer.put((double) i);
+template<typename T>
+void producer(RingBuffer<T>& buffer) {
+    for (uint32_t i = 0; i < 20; ++i) {
+        buffer.put(i);
         std::cout << "Produced: " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Simulate some work
+    }
+}
+
+template<typename T>
+void consumer(RingBuffer<T>& buffer) {
+
+    std::vector<T> items; 
+    for (uint32_t i = 0; i < 20; ++i) {
+        buffer.get(items);
+        const auto now = std::chrono::system_clock::now();
+        const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+        std::cout << std::ctime(&t_c) << "Consumed: ";
+
+        for (const T& i : items) {
+            std::cout << i << ' ';
+        }
+
+        std::cout << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Simulate some work
     }
 }
 
-void consumer(RingBuffer<int>& buffer) {
-    for (int i = 0; i < 20; ++i) {
-        int item = buffer.get();
-        std::cout << "Consumed: " << item << std::endl;
+class Foo {
+    int32_t i_;
+public:
+    Foo() : i_(0) {}
+    Foo(int i) {
+        i_ = (i);
+    } 
+
+    Foo operator+(Foo const& obj)
+    {
+        Foo res;
+        res.i_ = i_*100 + obj.i_;
+        return res;
     }
+
+    friend std::ostream& operator<<(std::ostream& os, const Foo& dt);
+};
+
+std::ostream& operator<<(std::ostream& os, const Foo& foo)
+{
+    os << "i_:" << foo.i_;
+    return os;
 }
 
 int main() {
-    RingBuffer<int> buffer;
+    RingBuffer<Foo> buffer;
 
-    std::thread producer_thread(producer, std::ref(buffer));
-    std::thread consumer_thread(consumer, std::ref(buffer));
+   std::thread producer_thread(producer<Foo>, std::ref(buffer));
+   std::thread consumer_thread(consumer<Foo>, std::ref(buffer));
 
-    producer_thread.join();
-    consumer_thread.join();
+   producer_thread.join();
+   consumer_thread.join();
+
+    // Foo a (5);  
+    // Foo b (6);  
+    // Foo c = a + b;
+    // std::cout << c << std::endl;
+
 
     return 0;
 }
